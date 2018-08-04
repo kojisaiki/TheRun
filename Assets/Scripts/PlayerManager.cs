6afad69193010e249cbb8ace1bcd09adbc1e2607
佐伯紘二 <saikou9901@gmail.com>
2018-08-04 15:58:38 +0900

    Implements jump action.

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -2,61 +2,95 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerManager : MonoBehaviour {
+public class PlayerManager : MonoBehaviour
+{
+    public LayerMask blockLayer;
 
-	private Rigidbody2D rbody;
+    private Rigidbody2D rbody;
 
-	private const float MOVE_SPEED = 3;
-	private float moveSpeed;
+    private const float MOVE_SPEED = 3;
+    private float moveSpeed;
+    private float jumpPower = 400;
+    private bool goJump = false;
+    private bool canJump = false;
 
-	public enum MOVE_DIR {
-		STOP,
-		LEFT,
-		RIGHT,
-	};
+    public enum MOVE_DIR
+    {
+        STOP,
+        LEFT,
+        RIGHT,
+    };
 
-	private MOVE_DIR moveDirection = MOVE_DIR.STOP;
+    private MOVE_DIR moveDirection = MOVE_DIR.STOP;
 
-	// Use this for initialization
-	void Start () {
-		rbody = GetComponent<Rigidbody2D> ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
 
-	private void FixedUpdate() {
-		switch (moveDirection)
-		{
-			case MOVE_DIR.STOP:
-				moveSpeed = 0;
-				break;
-			case MOVE_DIR.LEFT:
-				moveSpeed = MOVE_SPEED * -1;
-				transform.localScale = new Vector2 (-1, 1);
-				break;
-			case MOVE_DIR.RIGHT:
-				moveSpeed = MOVE_SPEED;
-				transform.localScale = new Vector2 (1, 1);
-				break;
-			default:
-				break;
-		}
+    // Update is called once per frame
+    void Update()
+    {
+        canJump =
+            Physics2D.Linecast(
+                transform.position - (transform.right * 0.3f),
+                transform.position - (transform.up * 0.1f),
+                blockLayer) ||
+            Physics2D.Linecast(
+                transform.position + (transform.right * 0.3f),
+                transform.position - (transform.up * 0.1f),
+                blockLayer);
+    }
 
-		rbody.velocity = new Vector2 (moveSpeed, rbody.velocity.y);
-	}
+    private void FixedUpdate()
+    {
+        switch (moveDirection)
+        {
+            case MOVE_DIR.STOP:
+                moveSpeed = 0;
+                break;
+            case MOVE_DIR.LEFT:
+                moveSpeed = MOVE_SPEED * -1;
+                transform.localScale = new Vector2(-1, 1);
+                break;
+            case MOVE_DIR.RIGHT:
+                moveSpeed = MOVE_SPEED;
+                transform.localScale = new Vector2(1, 1);
+                break;
+            default:
+                break;
+        }
 
-	public void PushLeftButton () {
-		moveDirection = MOVE_DIR.LEFT;
-	}
+        rbody.velocity = new Vector2(moveSpeed, rbody.velocity.y);
 
-	public void PushRightButton () {
-		moveDirection = MOVE_DIR.RIGHT;
-	}
+        if (goJump)
+        {
+            rbody.AddForce(Vector2.up * jumpPower);
+            goJump = false;
+        }
+    }
 
-	public void ReleaseMoveButton () {
-		moveDirection = MOVE_DIR.STOP;
-	}
+    public void PushLeftButton()
+    {
+        moveDirection = MOVE_DIR.LEFT;
+    }
+
+    public void PushRightButton()
+    {
+        moveDirection = MOVE_DIR.RIGHT;
+    }
+
+    public void ReleaseMoveButton()
+    {
+        moveDirection = MOVE_DIR.STOP;
+    }
+
+    public void PushJumpButton()
+    {
+        if (canJump)
+        {
+            goJump = true;
+        }
+    }
 }
